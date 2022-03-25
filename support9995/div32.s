fa4abc887167ca00c0deb8faea6d887cf152141e
Alan Cox <etchedpixels@gmail.com>
2022-03-25 17:50:23 +0000

    div32s: more work on divides

--- support9995/div32.s
+++ support9995/div32.s
@@ -6,6 +6,10 @@
 		.export div32i
 		.export	mod32
 		.export mod32i
+		.export divs32
+		.export divs32i
+		.export mods32
+		.export mods32i
 
 		.code
 
@@ -29,31 +33,31 @@ div32op_1:
 	mov	r2,r7		; save the bit
 	sla	r2, 1
 	sla	r3, 1
-	jnc	div32op_2
+	jnc	@div32op_2
 	inc	r2
 div32op_2:
 	inc	r3
 	; Shift the working register left
 	sla	r0, 1
 	sla	r1, 1
-	jnc	div32op_3
+	jnc	@div32op_3
 	inc	r0
-div32op_3;
+div32op_3:
 	; capture the old high bit of the dividend into the working register
 	andi	r7,1
 	a	r7,r1
 	; 32bit compare
 	c	r0, r4
-	jlo	div32op_5
-	jhi	div32op_4
+	jl	@div32op_5
+	jh	@div32op_4
 	c	r1,r5
-	jlo	div32op_5
+	jl	@div32op_5
 div32op_4:
 	; >=
 	inc	r1		; set the low bit of the working reg
-	sub	r1, r5		; safe as r1 can't be 0 at this point
-	jnc	div32op_5
-	sub	r0,r4
+	s	r1, r5		; safe as r1 can't be 0 at this point
+	jnc	@div32op_5
+	s	r0,r4
 div32op_5:
 	dec	r6
 	jne	@div32op_1
@@ -70,14 +74,17 @@ mod32i:
 mod32:
 	dect	r13
 	mov	r11,*r13
-	bl	div32
+	bl	@div32
 	mov	*r13+,r11
 	mov	r2,r0
 	mov	r3,r1	
 	rt
 
 ; The result shall be negative if the signs differ
-div32s:
+divs32i:
+	mov	*r11+,r2
+	mov	*r11+,r3
+divs32:
 	dect	r13
 	mov	r11,*r13
 	dect	r13
@@ -85,33 +92,36 @@ div32s:
 	dect	r13
 	mov	r5, *r13
 	clr	r5		; sign tracker
-	ci	r0, 0
-	jge	@div32s_1
+	ci	r0, 0x8000
+	jl	@divs32_1
 	bl	@neg32
 	inc	r5		; track sign
-div32s_1:
-	ci	r2, 0
-	jge	@div32s_3
+divs32_1:
+	ci	r2, 0x8000
+	jl	@divs32_3
 	; Negate r2/r3
 	inv	r2
 	inv	r3
 	inc	r3
-	jne	@div32s_2
+	jne	@divs32_2
 	inc	r0
-div32s_2:
+divs32_2:
 	inc	r5		; track sign
-div32s_3:
+divs32_3:
 	bl	@div32
 	srl	r5,1
-	jnc	div32s_3
+	jnc	@divs32_3
 	; negate r0/r1
 	mov	*r13+,r5
 	mov	*r13+,r4
 	mov	*r13+,r11
 	rt
 
+mods32i:
+	mov	*r11+,r2
+	mov	*r11+,r3
 ; Actually remainder for C. The sign shall be the sign of the dividend
-mod32s:
+mods32:
 	dect	r13
 	mov	r11,*r13
 	dect	r13
@@ -119,17 +129,17 @@ mod32s:
 	dect	r13
 	mov	r5, *r13
 	clr	r5		; sign tracker
-	ci	r0, 0
-	jge	@div32s_1
+	ci	r0, 0x8000
+	jl	@mods32_1
 	bl	@neg32
 	; todo - negate r0/r1
 	inc	r5		; track sign
-mod32s_1:
+mods32_1:
 	bl	@div32
 	srl	r5,1
-	jnc	mod32s_2
+	jnc	@mods32_2
 	bl	@neg32
-mod32s_2:
+mods32_2:
 	mov	*r13+,r5
 	mov	*r13+,r4
 	mov	*r13+,r11
